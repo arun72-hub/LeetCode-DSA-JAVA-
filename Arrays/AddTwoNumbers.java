fe974430d301ea6365f26e946a4fa95d7b88a6fe
ARUN PRAKASH CHAURASIYA <arunprakash979268@gmail.com>
2026-09-19 23:13:01 +0530

    Add Question 2 - Add Two Numbers

---
+++ Arrays/AddTwoNumbers.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class AddTwoNumbers {
+
+    // Add two linked lists
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            current.next = new ListNode(digit);
+            current = current.next;
+        }
+
+        return dummy.next;
+    }
+
+    // Create linked list from user input
+    public static ListNode createList(Scanner sc) {
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        ListNode head = null;
+        ListNode tail = null;
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+
+            int value = sc.nextInt();
+
+            ListNode newNode = new ListNode(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        return head;
+    }
+
+    // Print linked list
+    public static void printList(ListNode head) {
+
+        while (head != null) {
+
+            System.out.print(head.val);
+
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First List");
+        ListNode l1 = createList(sc);
+
+        System.out.println("\nEnter Second List");
+        ListNode l2 = createList(sc);
+
+        ListNode result = addTwoNumbers(l1, l2);
+
+        System.out.println("\nFirst List:");
+        printList(l1);
+
+        System.out.println("Second List:");
+        printList(l2);
+
+        System.out.println("Sum:");
+        printList(result);
+
+        sc.close();
+    }
+}
+
+//LeetCode Add Two Numbers ke logic par hai. Us problem mein list ko reverse digits maana jata hai
