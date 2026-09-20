06c49d37db0c0e855566c58cca24befd0b18499a
ARUN PRAKASH CHAURASIYA <arunprakash979268@gmail.com>
2026-09-20 09:43:31 +0530

    Longest Substring Without Repeat Solution

---
+++ Arrays/LSWRepeat.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class LSWRepeat {
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter a string: ");
+    String s = sc.nextLine();
+
+    int answer = lengthOfLongestSubstring(s);
+
+    System.out.println("Longest substring length: " + answer);
+
+    sc.close();
+}
+    // public static void main(String[] args) {
+
+    //     String s = "abcabcbb";
+
+    //     int answer = lengthOfLongestSubstring(s);
+
+    //     System.out.println("Longest substring length: " + answer);
+    // }
+}
